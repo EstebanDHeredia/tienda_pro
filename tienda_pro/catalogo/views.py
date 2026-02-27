b471daafab03440efdf53a2a73eb3b301a0c1d3d
Esteban Heredia <estebanheredia@msn.com>
2026-02-27 12:48:54 -0300

    Terminada paginacion, filtro por categiria y ordenamiento por precio. También mejorada pagina de lista de productos con mejor presentacion en tarjetas

--- tienda_pro/catalogo/views.py
+++ tienda_pro/catalogo/views.py
@@ -8,16 +8,40 @@ class ListaProductosView(ListView):
     model = Producto
     template_name = 'catalogo/lista.html'
     context_object_name = 'mis_productos'
+    paginate_by = 6
     
     def get_queryset(self):
         queryset = Producto.objects.filter(stock__gt=0)
-        
+       
+        # 1. Filtro de búsqueda
         termino = self.request.GET.get('buscar')
 
         if termino:
             queryset = queryset.filter(
                 Q(nombre__icontains=termino) | Q(descripcion__icontains=termino))
+
+        # 2. Filtro de categoría
+        categoria_id = self.request.GET.get('categoria')
+        if categoria_id:
+            queryset = queryset.filter(categoria_id=categoria_id)
+        
+        # 3. Ordenamiento
+        orden = self.request.GET.get('orden')
+        if orden == 'barato':
+            queryset = queryset.order_by('precio')
+        elif orden == 'caro':
+            queryset = queryset.order_by('-precio')
+        else:
+            # Usamos '-id' para que los últimos productos añadidos salgan primero
+            queryset = queryset.order_by('-id') 
+
         return queryset
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        from .models import Categoria
+        context['categorias'] = Categoria.objects.all()
+        return context
         
 
 class DetalleProductoView(DetailView):
